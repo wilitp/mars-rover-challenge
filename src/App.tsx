832e95810fb6aeef7efa2ba643b7895d13bd27bd
wilitp <guilledeipola@gmail.com>
2022-01-15 01:05:46 -0300

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,194 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  ChangeEvent,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { roverPhotos } from "./api";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { SelectChangeEvent, styled } from "@mui/material";
+import Cockpit from "./components/Cockpit/Cockpit";
+import {
+  Bookmarks,
+  getBookmarks,
+  saveBookmark,
+  deleteBookmark,
+  Bookmark,
+} from "./bookmarks";
 
 function App() {
+  // Bookmarks' state
+  const [bookmarks, setBookmarks] = useState<Bookmarks>({});
+
+  // Controls' state
+  const [camera, setCamera] = useState<string>("all");
+  const [date, setDate] = useState<string>(
+    new Date().toISOString().slice(0, 10)
+  );
+  const [sol, setSol] = useState<string>("1");
+  const [useSol, setUseSol] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [rover, setRover] = useState<string>("curiosity");
+
+  // Photos' state
+  const [photos, setPhotos] = useState<{ img_src: string }[] | null>(null);
+
+  // Did we reach the bottom?
+  const [bottomed, setBottomed] = useState<boolean>(false);
+
+  // Are we fetching photos?
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+
+  useEffect(() => {
+    setBookmarks(getBookmarks());
+  }, []);
+
+  // Is it the first render?
+  const firstRender = useRef(true);
+  // Fetch more photos on page change
+  useEffect(() => {
+    // Won't run on the first render
+    // debugger;
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+    setIsFetching(true);
+    roverPhotos(page, camera, rover, undefined, sol).then((data) => {
+      if (!photos) {
+        setPhotos(data);
+        return;
+      }
+      if (data.length === 0) {
+        setBottomed(true);
+        setIsFetching(false);
+        return;
+      }
+      const newPhotos = photos.concat(data);
+      console.log(newPhotos.length);
+      setPhotos(newPhotos);
+      setIsFetching(false);
+    });
+  }, [page]);
+
+  // Fetch and display on new selection
+  useEffect(() => {
+    setIsFetching(true);
+    setBottomed(false);
+    if (useSol) {
+      roverPhotos(page, camera, rover, undefined, sol).then((data) => {
+        setPhotos(data);
+        setIsFetching(false);
+      });
+    } else {
+      roverPhotos(page, camera, rover, date).then((data) => {
+        setPhotos(data);
+        setIsFetching(false);
+      });
+    }
+  }, [camera, date, sol, useSol, rover]);
+
+  const images = useMemo(() => {
+    if (!photos) return;
+    return photos.map((p) => (
+      <img style={{ display: "block" }} key={p.img_src} src={p.img_src}></img>
+    ));
+  }, [photos]);
+
+  function handleCameraChange(e: SelectChangeEvent) {
+    setCamera(e.target.value);
+  }
+
+  function handleDateChange(e: ChangeEvent<HTMLInputElement>) {
+    setDate(e.target.value);
+  }
+
+  function handleSolChange(e: ChangeEvent<HTMLInputElement>) {
+    setSol(e.target.value);
+  }
+
+  function handleUseSolChange(e: ChangeEvent<HTMLInputElement>) {
+    setUseSol(e.target.checked);
+  }
+
+  function handleRoverChange(e: SelectChangeEvent<string>) {
+    setRover(e.target.value);
+  }
+
+  const StyledStack = styled(Stack)`
+    max-width: 800px;
+    margin: auto;
+  `;
+
+  function addBookmark() {
+    const bk: Bookmark = {
+      useSol,
+      rover,
+      date,
+      sol,
+      camera,
+    };
+    const bkms = saveBookmark(bk);
+    setBookmarks(bkms);
+  }
+
+  function removeBookmark(id: string) {
+    const bkms = deleteBookmark(id);
+    setBookmarks(bkms);
+  }
+
+  function loadBookmark(bk: Bookmark) {
+    setUseSol(bk.useSol);
+    setRover(bk.rover);
+    setDate(bk.date);
+    setCamera(bk.camera);
+    setSol(bk.sol);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ maxWidth: "600px" }}>
+      <Typography variant="h2" component="h1">
+        Mars Rovers' Photos
+      </Typography>
+      <Cockpit
+        loadBookmark={loadBookmark}
+        removeBookmark={removeBookmark}
+        addBookmark={addBookmark}
+        bookmarks={bookmarks}
+        rover={rover}
+        date={date}
+        sol={sol}
+        camera={camera}
+        useSol={useSol}
+        handleCameraChange={handleCameraChange}
+        handleRoverChange={handleRoverChange}
+        handleDateChange={handleDateChange}
+        handleUseSolChange={handleUseSolChange}
+        handleSolChange={handleSolChange}
+      />
+      {!images ? (
+        <p>Loading...</p>
+      ) : images.length === 0 ? (
+        <Typography variant="body1">No photos available</Typography>
+      ) : (
+        <StyledStack spacing={2}>
+          {images as any}
+
+          {!bottomed ? (
+            <Button variant="text" onClick={() => setPage(page + 1)}>
+              Load More
+            </Button>
+          ) : null}
+          {isFetching && !bottomed ? (
+            <Typography variant="body1">Loading more...</Typography>
+          ) : null}
+        </StyledStack>
+      )}
+    </Container>
   );
 }
 
---
+++ src/components/Cockpit/Cockpit.tsx
@@ -0,0 +1,202 @@
+import React, { FC } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import Switch from "@mui/material/Switch";
+import Box from "@mui/material/Box";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Bookmarks } from "../../bookmarks";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+
+interface CockpitProps {
+  useSol: boolean;
+  rover: string;
+  sol: string;
+  date: string;
+  camera: string;
+  bookmarks: Bookmarks;
+  addBookmark: Function;
+  loadBookmark: Function;
+  removeBookmark: Function;
+  handleDateChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleUseSolChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleSolChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleRoverChange: (e: SelectChangeEvent) => void;
+  handleCameraChange: (e: SelectChangeEvent) => void;
+}
+
+const rovers = ["curiosity", "spirit", "opportunity"];
+const cameras = {
+  fhaz: "Front Hazard Avoidance Camera",
+  rhaz: "Rear Hazard Avoidance Camera",
+  mast: "Mast Camera",
+  chemcam: "Chemistry and Camera Complex",
+  mahli: "Mars Hand Lens Imager",
+  mardi: "Mars Descent Imager",
+  navcam: "Navigation Camera",
+  pancam: "Panoramic Camera",
+  minites: "Miniature Thermal Emission Spectrometer (Mini-TES)",
+};
+const label = { inputProps: { "aria-label": "controlled" } };
+
+const Cockpit: FC<CockpitProps> = ({
+  date,
+  rover,
+  sol,
+  useSol,
+  camera,
+  bookmarks,
+  removeBookmark,
+  loadBookmark,
+  addBookmark,
+  handleDateChange,
+  handleUseSolChange,
+  handleSolChange,
+  handleRoverChange,
+  handleCameraChange,
+}) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const noBookmarks = Object.keys(bookmarks).length === 0;
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (noBookmarks) return;
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleBookmarkDeletion = (b: string) => (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    e.stopPropagation();
+    removeBookmark(b);
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box marginTop={3} marginBottom={3}>
+      <Box marginBottom={1}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={useSol}
+              {...label}
+              value={useSol}
+              onChange={handleUseSolChange}
+            />
+          }
+          label="Use Sol"
+        />
+      </Box>
+      <Box marginBottom={1}>
+        <TextField
+          value={date}
+          type="date"
+          label="Date"
+          id="date-input"
+          onChange={handleDateChange}
+          disabled={useSol}
+        />
+        <TextField
+          value={sol}
+          onChange={handleSolChange}
+          type="number"
+          label="Sol"
+          defaultValue={1}
+          id="sol-input"
+          disabled={!useSol}
+        />
+      </Box>
+      <Box marginBottom={1}>
+        <FormControl>
+          <InputLabel id="camera-select-label">Rover</InputLabel>
+          <Select
+            labelId="camera-select-label"
+            id="camera-select"
+            value={rover}
+            label="Rover"
+            onChange={handleRoverChange}
+          >
+            {rovers.map((r) => (
+              <MenuItem value={r} key={r}>
+                {r.slice(0, 1).toUpperCase() + r.slice(1)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel id="camera-select-label">Camera</InputLabel>
+          <Select
+            labelId="camera-select-label"
+            id="camera-select"
+            value={camera}
+            label="Camera"
+            onChange={handleCameraChange}
+          >
+            <MenuItem value={"all"}>All</MenuItem>
+            {Object.keys(cameras).map((k) => (
+              <MenuItem value={k} key={k}>
+                {cameras[k as keyof typeof cameras]}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {noBookmarks ? null : (
+          <div>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              Bookmarks
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              {Object.keys(bookmarks).map((b) => (
+                <MenuItem
+                  value={b}
+                  key={b}
+                  onClick={() => loadBookmark(bookmarks[b])}
+                >
+                  {`${bookmarks[b].rover} - ${
+                    bookmarks[b].useSol ? bookmarks[b].sol : bookmarks[b].date
+                  }`}{" "}
+                  <div style={{ marginLeft: "auto" }}>
+                    <CloseIcon
+                      onClick={handleBookmarkDeletion(b) as any}
+                      component="svg"
+                    />
+                  </div>
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        )}
+      </Box>
+      <Box>
+        <Button onClick={addBookmark as any} variant="contained">
+          Bookmark current
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Cockpit;
